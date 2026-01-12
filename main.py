add7bd7c435b1f9ff8da66b1b88c4b23631cd9b9
Tiago dos Santos <tiago2904santos@gmail.com>
2026-01-12 12:01:23 -0300

    feat: implement CRUD operations for Estudante model with FastAPI

---
+++ main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from typing import List
+import models
+import schemas
+from database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+def get_db():
+    db = SessionLocal
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/estudantes/", 
+          response_model=schemas.EstudanteResponse)
+
+def create_student(
+        student: schemas.EstudanteCreate,
+        db: Session = Depends(get_db)):
+    db_student = models.Estudante(**student.model_dump())
+    db.add(db_student)
+    db.commit()
+    db.refresh(db_student)
+    return db_student
+
+@app.get("/estudantes/", response_model=List[schemas.EstudanteResponse])
+def read_students(db: Session = Depends(get_db())):
+    students = db.query(models.Estudante).all()
+    return students
